d951342e806bbc57910c34a205007bc8a818a755
svasenkov <aanher@gmail.com>
2021-11-22 21:13:44 +0300

    added ui test

--- src/test/java/tests/simple/NegativeTests.java
+++ src/test/java/tests/simple/NegativeTests.java
@@ -1,3 +1,5 @@
+package tests.simple;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
--- src/test/java/tests/simple/SkippedTests.java
+++ src/test/java/tests/simple/SkippedTests.java
@@ -1,3 +1,5 @@
+package tests.simple;
+
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
